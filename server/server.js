58a88905b26f0de378d277aefbf907cc3725bdb0
bernardfernando <jagathf@gmail.com>
2023-07-27 15:20:07 +0100

    amends - tims code

--- server/server.js
+++ server/server.js
@@ -5,11 +5,14 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 
-const survive = require("./library/survive");
+const survive = require("./library/survive.js");
 const maths = require("./library/maths");
 
-app.get("/survive", survive);
+app.get("/", function (request, response) {
+  response.json("You are looking at my root route. How roude");
+});
 
+app.get("/survive", survive);
 app.get("/maths", maths);
 
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
