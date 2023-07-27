3f731e57d8ccf80b9bf5a5c1116ea1fc8e6fc37e
bernardfernando <jagathf@gmail.com>
2023-07-27 14:03:20 +0100

    amend

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+const app = express();
+app.use(cors());
+
+const survive = require("./library/survive");
+const maths = require("./library/maths");
+
+app.get("/survive", survive);
+
+app.get("/maths", maths);
+
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
